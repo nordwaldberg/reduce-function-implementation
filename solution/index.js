76261d2f06a8d3bfeba85afb4186826b99a02bdd
nordwaldberg <nord.waldberg@gmail.com>
2021-10-15 13:56:46 +0300

    First version of reduce was created

---
+++ solution/index.js
@@ -0,0 +1,24 @@
+module.exports = function (Homework) {
+    async function reduceLoop (array, fn, initialValue) {
+        let accumulator = initialValue ? initialValue : 0;
+
+        const arrayLength = await new Promise(resolve => array.length(resolve));
+        let index = 0;
+        let condition = await new Promise(resolve => Homework.less(index, arrayLength, resolve));
+
+        while (condition) {
+            let current = await new Promise(resolve => array.get(index, resolve));
+            accumulator = await new Promise(resolve => {
+                fn(accumulator, current, index, array, resolve);
+            });
+            index++;
+            condition = await new Promise(resolve =>  Homework.less(index, arrayLength, resolve));
+        }
+
+        return accumulator;
+    }
+
+    return (array, fn, initialValue, cb) => {
+        reduceLoop(array, fn, initialValue).then(result => cb(result));
+    }
+}
